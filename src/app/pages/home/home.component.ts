373c909beb7caa56d29ccc20c1d63319b3a36d48
mateus-msantos-dev <mateus.msantos.dev@gmail.com>
2025-11-25 00:42:27 -0300

    Página home

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,59 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit, OnDestroy {
 
+  menuOpen = false;
+  userMenuOpen = false;
+
+  constructor(private router: Router) {}
+
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
+  }
+
+  toggleUserMenu(event: Event) {
+    event.stopPropagation();
+    this.userMenuOpen = !this.userMenuOpen;
+  }
+
+  logout() {
+    this.userMenuOpen = false;
+    this.router.navigate(['/login']);
+  }
+
+  closeAllMenus() {
+    this.menuOpen = false;
+    this.userMenuOpen = false;
+  }
+
+  ngOnInit() {
+    document.addEventListener('click', this.handleClickOutside);
+  }
+
+  ngOnDestroy() {
+    document.removeEventListener('click', this.handleClickOutside);
+  }
+
+  handleClickOutside = (event: any) => {
+    const userMenu = document.querySelector('.logout-menu');
+    const userButton = document.querySelector('.botao-usuario');
+
+    if (!userMenu || !userButton) return;
+
+    const clickedInsideMenu = userMenu.contains(event.target);
+    const clickedButton = userButton.contains(event.target);
+
+    if (!clickedInsideMenu && !clickedButton) {
+      this.userMenuOpen = false;
+    }
+  };
 }
